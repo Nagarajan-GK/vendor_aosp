3dbfccc5fa0223bea0b100cf42621f2aa40e6740
Stallix <thestallix@gmail.com>
2019-04-21 17:46:51 +0530

    vendor: Add 'Source Sans Pro' font family
    Taken from Android Q Beta 2

--- config/fonts.mk
+++ config/fonts.mk
@@ -10,4 +10,10 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/fonts/roadrage/road_rage.ttf:system/fonts/RoadRage-Regular.ttf \
     vendor/aosp/prebuilt/fonts/snowstorm/snowstorm.ttf:system/fonts/Snowstorm-Regular.ttf \
     vendor/aosp/prebuilt/fonts/vcrosd/vcr_osd_mono.ttf:system/fonts/ThemeableFont-Regular.ttf \
-    vendor/aosp/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf
+    vendor/aosp/prebuilt/fonts/Shamshung/Shamshung.ttf:system/fonts/Shamshung.ttf \
+    vendor/aosp/prebuilt/fonts/SourceSansPro/SourceSansPro-Regular.ttf:system/fonts/SourceSansPro-Regular.ttf \
+    vendor/aosp/prebuilt/fonts/SourceSansPro/SourceSansPro-Italic.ttf:system/fonts/SourceSansPro-Italic.ttf \
+    vendor/aosp/prebuilt/fonts/SourceSansPro/SourceSansPro-SemiBold.ttf:system/fonts/SourceSansPro-SemiBold.ttf \
+    vendor/aosp/prebuilt/fonts/SourceSansPro/SourceSansPro-SemiBoldItalic.ttf:system/fonts/SourceSansPro-SemiBoldItalic.ttf \
+    vendor/aosp/prebuilt/fonts/SourceSansPro/SourceSansPro-Bold.ttf:system/fonts/SourceSansPro-Bold.ttf \
+    vendor/aosp/prebuilt/fonts/SourceSansPro/SourceSansPro-BoldItalic.ttf:system/fonts/SourceSansPro-BoldItalic.ttf
